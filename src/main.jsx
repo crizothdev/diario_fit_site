319fa2fe908f1f9ccb2d02e6b53d1b1736a21559
crizothdev <crizoth.dev@gmail.com>
2026-05-22 17:02:37 -0300

    adjust do run in dev

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/diario_fit_site">
       <App />
     </BrowserRouter>
   </StrictMode>,
